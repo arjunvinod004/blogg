73e50ac69deecfea2b94fc942f1a17d4d9178e3d
arjunvinod004 <arjunvt004@gmail.com>
2023-06-30 07:35:55 +0530

    blog

---
+++ src/StaticData/UserData.js
@@ -0,0 +1,41 @@
+export const UserData = [
+    {
+      id: 1,
+      name: 'smith',
+      job: 'web developer',
+      image:
+        'https://images.generated.photos/43N1jLqgRA0Q7_gXIEEVtNsNjrkM0TWdPNOGDjjrpKI/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODA5MjMxLmpwZw.jpg',
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: ' johnson',
+      job: 'web designer',
+      image:
+        'https://images.generated.photos/GIGaLRE-wNwBjovdWNfOaUtmlrE3wDJJiR03vBsjO5w/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTU2Mzc5LmpwZw.jpg',
+      text:
+        'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter',
+      job: 'intern',
+      image:
+        'https://images.generated.photos/bxfZR9ndpIgduCYbmiV8GOUZEi2vaSbB-g6GAq1daM4/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Nzk2NDgwLmpwZw.jpg',
+      text:
+        'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: ' anderson',
+      job: 'the boss',
+      image:
+        'https://images.generated.photos/sqx7F7_gmRQGf2RxKR9xCYwNy8GbLJQ2u3OZ8Q4wM-8/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTEyNjQ1LmpwZw.jpg',
+      text:
+        'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+  
+ 
+  
